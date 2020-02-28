407ff2746e3fab73ba79de222f0f6a8f15b1db1c
Sujit Muduli <smuduli@iitk.ac.in>
2020-02-29 01:10:06 +0530

    new test point for IMPLIES Operator

---
+++ property-parser-testsuite/src/test_implies_operator.cpp
@@ -0,0 +1,28 @@
+#include <memory>
+#include <stdlib.h>
+#include "property_parser.h"
+
+#include <gtest/gtest.h>
+
+TEST(PropertyParserTest, ValidTraceIMPLIESVacuous) {
+
+    num_lit = 1;
+    tr_lit.clear();
+    tr_lit.emplace_back("x");
+
+    Formula *formula = parse("(G (IMPLIES (EQL (1.x) (1)) (EQL (2.x) (2))))");
+
+    // creating test with 1.x always zero making (1.x == 1) always false
+    // hence, the formula always holds
+
+    long tr1[] = {0};
+    long tr2[] = {1};
+    bool result = false;
+
+    for(unsigned idx = 0; idx < 10; ++idx) {
+      result = formula->eval(tr1, tr2);
+    }
+
+    EXPECT_TRUE(result);
+    
+}
