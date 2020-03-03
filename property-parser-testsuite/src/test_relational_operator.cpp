1cc37230e66b303ecefc36e2f02692fcee0ad896
Sujit Muduli <smuduli@iitk.ac.in>
2020-03-03 17:20:59 +0530

    added some more tests for EQ operator

--- property-parser-testsuite/src/test_relational_operator.cpp
+++ property-parser-testsuite/src/test_relational_operator.cpp
@@ -9,72 +9,106 @@
 
 TEST(PropertyParserTest, ValidTraceLEQ) {
 
-    num_lit = 1;
-    tr_lit.clear();
-    tr_lit.emplace_back("x");
+	num_lit = 1;
+	tr_lit.clear();
+	tr_lit.emplace_back("x");
     
-    Formula *formula = parse("(O (LEQ (1.x) (2.x)))");
-    bool result = false;
-    const long tr[] = {1, 2};
+	Formula *formula = parse("(O (LEQ (1.x) (2.x)))");
+	bool result = false;
+	const long tr[] = {1, 2};
 
-    ProbModel pm;
+	ProbModel pm;
 
-    for (unsigned idx = 0; idx < 100; ++idx) {
+	for (unsigned idx = 0; idx < 100; ++idx) {
 
-	if(pm.probM(20)) {
-	    result = formula->eval(&tr[0], &tr[1]);
+		if(pm.probM(20)) {
+			result = formula->eval(&tr[0], &tr[1]);
+		}
+		else {
+			result = formula->eval(&tr[pm.probM(50)], &tr[pm.probM(50)]);
+		}
 	}
-	else {
-	    result = formula->eval(&tr[pm.probM(50)], &tr[pm.probM(50)]);
-	}
-    }
     
-    EXPECT_TRUE(result);
+	EXPECT_TRUE(result);
 }
 
 
 TEST(PropertyParserTest, ValidTraceLEQ_2Literals) {
 
-    num_lit = 2;
-    tr_lit.clear();
-    tr_lit.emplace_back("x");
-    tr_lit.emplace_back("y");
+	num_lit = 2;
+	tr_lit.clear();
+	tr_lit.emplace_back("x");
+	tr_lit.emplace_back("y");
     
-    Formula *formula = parse("(O (AND (LEQ (1.x) (2.x)) (GEQ (1.y) (2.y))))");
-    bool result = false;
-    const long tr[] = {1, 2};
+	Formula *formula = parse("(O (AND (LEQ (1.x) (2.x)) (GEQ (1.y) (2.y))))");
+	bool result = false;
+	const long tr[] = {1, 2};
 
-    ProbModel pm;
+	ProbModel pm;
+	const unsigned validIdx = 20;
 
-    
-    
-    // EXPECT_TRUE(result);
+	for(unsigned idx = 0; idx < 100; ++idx) {
+		if (idx == validIdx) {
+			result = formula->eval(&tr[0], &tr[1]);
+			result = formula->eval(&tr[1], &tr[0]);
+		}
+		else {
+			result = formula->eval(&tr[pm.probM(50)], &tr[pm.probM(50)]);
+			result = formula->eval(&tr[pm.probM(50)], &tr[pm.probM(50)]);
+		}
+	}
+        
+	EXPECT_TRUE(result);
 }
 
 
 TEST(PropertyParserTest, InvalidTraceEQ) {
 
-    num_lit = 1;
-    tr_lit.clear();
-    tr_lit.emplace_back("x");
+	num_lit = 1;
+	tr_lit.clear();
+	tr_lit.emplace_back("x");
     
-    Formula *formula = parse("(O (LEQ (1.x) (2.x)))");
-    bool result = false;
-    const long tr[] = {1, 2};
+	Formula *formula = parse("(O (LEQ (1.x) (2.x)))");
+	bool result = false;
+	const long tr[] = {1, 2};
 
+	ProbModel pm;
+
+	for (unsigned idx = 0; idx < 100; ++idx) {
+
+		if(pm.probM(20)) {
+			result = formula->eval(&tr[1], &tr[0]);
+		}
+		else {
+			int randIdx = rand() % 2;
+			result = formula->eval(&tr[randIdx], &tr[randIdx]);
+		}
+	}
+    
 }
 
 TEST(PropertyParserTest, InvalidTraceEQ_2Literals) {
 
-    num_lit = 2;
-    tr_lit.clear();
-    tr_lit.emplace_back("x");
-    tr_lit.emplace_back("y");
+	num_lit = 2;
+	tr_lit.clear();
+	tr_lit.emplace_back("x");
+	tr_lit.emplace_back("y");
     
-    Formula *formula = parse("(O (AND (LEQ (1.x) (2.x)) (GEQ (1.y) (2.y))))");
-    bool result = false;
-    const long tr[] = {1, 2};
+	Formula *formula = parse("(O (AND (LEQ (1.x) (2.x)) (GEQ (1.y) (2.y))))");
+	bool result = false;
+	const long tr[] = {1, 2};
+	ProbModel pm;
 
+	// FIXME : check how to use the API for two_literals
+	// tests may be failing due to wrong usage of the API
+	for(unsigned idx = 0; idx < 100; ++idx) {
+		std::cout << tr[0] << " : " << tr[0] << std::endl;
+		result = formula->eval(&tr[0], &tr[0]);
+		std::cout << tr[1] << " : " << tr[1] << std::endl;
+		result = formula->eval(&tr[1], &tr[1]);
+	}
+
+	EXPECT_FALSE(result);
 }
 
 
