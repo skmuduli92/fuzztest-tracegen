b3ee67629422f7fbc2838c0165404d1dbf3da5fd
Sujit Kumar Muduli <smuduli@iitk.ac.in>
2020-03-11 14:25:12 +0530

    testing yesterday operator

--- libprop/test/testprop.cpp
+++ libprop/test/testprop.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <iomanip>
 #include <trace.h>
-#include <formula.h>
-
 #include <cstdlib>
+
+#include <gtest/gtest.h>
+
+#include "formula.h"
+
+using namespace HyperPLTL;
+using namespace std;
+
 
 std::pair<PTrace, PTrace> createSatTraces1(unsigned xIndex, unsigned yIndex)
 {
-  using namespace std;
-
   assert (xIndex == 0);
   assert (yIndex == 1);
   PTrace trace1(new Trace(2));
@@ -72,8 +76,100 @@ void test1()
   auto traces = createSatTraces1(xIndex, yIndex);
 }
 
-int main(int argc, char* argv[])
-{
-  test1();
-  return 0;
+TEST(TestPropLib, TestALWAYSSimple) {
+  
+  // Formula: G(x.1 = x.2) => G(y.1 = y.2)
+  PVarMap varmap(new VarMap());
+  // x
+  unsigned xIndex = varmap->addVar("x");
+  PTerm x(new TermVar(varmap, xIndex));
+  // y
+  unsigned yIndex = varmap->addVar("y");
+  PTerm y(new TermVar(varmap, yIndex));
+  // (eq x)
+  PHyperProp eqX(new Equal(varmap, x));
+  // (eq y)
+  PHyperProp eqY(new Equal(varmap, y));
+  // (G (eq x))
+  PHyperProp GeqX(new Always(varmap, eqX));
+  // (G (eq y))
+  PHyperProp GeqY(new Always(varmap, eqY));
+  // (G (eq x)) => (G (eq y))
+  PHyperProp F(new Implies(varmap, GeqX, GeqY));
+  cout << "formula: "; F->display(cout); cout << endl;
+  
+  bool result = false;
+
+  auto traces = createSatTraces1(xIndex, yIndex);
+  TraceList tracelist({traces.first, traces.second});
+
+  for (size_t cycle = 0; cycle < traces.first->length(); ++cycle) {
+    result = F->eval(cycle, tracelist);
+  }
+
+  EXPECT_TRUE(result);
+}
+
+TEST(TestPropLib, TestYesterdaySimple) {
+
+  PVarMap varmap(new VarMap());
+  // x
+  unsigned xIndex = varmap->addVar("x");
+  PTerm x(new TermVar(varmap, xIndex));
+  // y
+  unsigned yIndex = varmap->addVar("y");
+  PTerm y(new TermVar(varmap, yIndex));
+  // (eq x)
+  PHyperProp eqX(new Equal(varmap, x));
+  // (eq y)
+  PHyperProp eqY(new Equal(varmap, y));
+
+  // (Y (eq x))
+  PHyperProp YesterdayEqX(new Yesterday(varmap, eqX));
+  
+  // // (G (eq y))
+  // PHyperProp GeqY(new Always(varmap, eqY));
+
+  // (Y (eq x)) => (eq y)
+  PHyperProp F(new Implies(varmap, YesterdayEqX, eqY));
+  F->display(std::cout); std::cout << std::endl;
+  bool result = true;
+
+  PTrace trace1(new Trace(2));
+  PTrace trace2(new Trace(2));
+
+  
+  TraceList tracelist({trace1, trace2});
+
+  size_t cycle = 0;
+  for ( ; cycle < 10; ++cycle) {
+      trace1->updateTermValue(0, cycle, rand() % 10);
+      trace1->updateTermValue(1, cycle, rand() % 10);
+      trace2->updateTermValue(0, cycle, rand() % 10);
+      trace2->updateTermValue(1, cycle, rand() % 10);
+
+      result = F->eval(cycle, tracelist);
+  }
+
+    trace1->updateTermValue(0, cycle, 20);
+    trace1->updateTermValue(1, cycle, rand() % 10);
+    trace2->updateTermValue(0, cycle, 20);
+    trace2->updateTermValue(1, cycle, rand() % 10);
+
+    result = F->eval(cycle, tracelist);
+    cycle = cycle + 1;
+
+    trace1->updateTermValue(0, cycle, rand() % 10);
+    trace2->updateTermValue(0, cycle, rand() % 10);
+    trace1->updateTermValue(1, cycle, 31);
+    trace2->updateTermValue(1, cycle, 30);
+    result = F->eval(cycle, tracelist);
+
+    std::cout << "actual result : " << std::endl;
+    EXPECT_TRUE(result);    
+}
+
+int main(int argc, char* argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
