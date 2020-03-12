4ea6d17ae01445f87232ae94b34f6dc359ca9e64
Sujit Kumar Muduli <smuduli@iitk.ac.in>
2020-03-12 14:43:44 +0530

    added negative test-2 for SINCE operator

--- libprop/test/test_since_operator.cpp
+++ libprop/test/test_since_operator.cpp
@@ -78,6 +78,7 @@ TEST(PropertyLIbTest, ValidTraceSinceOperator) {
 
 TEST(PropertyLIbTest, InvalidTraceSinceOperator_Test1) {
 
+  // eqX never becomes true, hence SINCE(eqY, eqX) should fail.
   PHyperProp property = propertySinceOperator();
 
   PTrace trace1(new Trace(2));
@@ -99,6 +100,40 @@ TEST(PropertyLIbTest, InvalidTraceSinceOperator_Test1) {
     trace2->updateTermValue(0, cycle, xvalue);
     // setting 'y' var value
     trace1->updateTermValue(1, cycle, rand() % 100);
+    trace2->updateTermValue(1, cycle, rand() % 100);
+
+    result = property->eval(cycle, tracelist);
+  }
+ 
+  EXPECT_FALSE(result);  
+}
+
+TEST(PropertyLIbTest, InvalidTraceSinceOperator_Test2) {
+
+  // eqX is always true but eqY never becomes true
+  PHyperProp property = propertySinceOperator();
+
+  PTrace trace1(new Trace(2));
+  PTrace trace2(new Trace(2));
+  TraceList tracelist({trace1, trace2});
+
+  property->display(std::cout); std::cout << std::endl;
+  bool result = false;
+
+  size_t cycle = 0;
+  unsigned xvalue = 0;
+  unsigned yvalue = 0;
+  unsigned traceLength = rand() % 20 + 20;
+  
+  for(; cycle < traceLength; ++cycle) {
+    xvalue = rand() % 100;
+    // setting 'x' var value
+    trace1->updateTermValue(0, cycle, xvalue);
+    trace2->updateTermValue(0, cycle, xvalue);
+    // setting 'y' var value
+    yvalue = rand() % 100;
+    trace1->updateTermValue(1, cycle, rand() % 100);
+    yvalue = (yvalue == 50) ? 51 : (100 - yvalue);	
     trace2->updateTermValue(1, cycle, rand() % 100);
 
     result = property->eval(cycle, tracelist);
