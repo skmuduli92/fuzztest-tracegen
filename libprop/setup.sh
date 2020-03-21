5d4a797436915a77b451aaca2e74af9d1ef7052c
Sujit Kumar Muduli <smuduli@iitk.ac.in>
2020-03-22 01:35:38 +0530

    updated setup script and some comments

--- libprop/setup.sh
+++ libprop/setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# setting up google-test framework
 sudo apt-get install googletest libpthread-workqueue0
 cd /usr/src/googletest
 sudo cmake .
 sudo make
 sudo make install
+
+# installing boost libraries for boost::spirit::x3 parser
+sudo apt-get install libboost-all-dev
+
