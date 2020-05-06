58f1ba2f213262c66083789a6a24cb733c84a5e1
Sujit Kumar Muduli <smuduli@iitk.ac.in>
2020-05-06 20:58:17 +0530

    removing old libprop folder

--- libprop/include/formula_util.h
+++
@@ -1,9 +0,0 @@
-#ifndef __FORMULA_UTIL_H__
-#define __FORMULA_UTIL_H__
-
-#include "formula.h"
-#include "trace.h"
-
-bool evaluateTraces(HyperPLTL::PHyperProp formula, TraceList const& traces);
-
-#endif
